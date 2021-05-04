e2b5698d8216ac75788fbf4cb5ba1dcc7a73c027
Chris Chuang <chrischuang0421@gmail.com>
2021-05-04 14:10:18 +0800

    Complete exercise 02,03

--- src/00/index.js
+++ src/00/index.js
@@ -1,17 +1,60 @@
-import { createMachine } from 'xstate';
+import { createMachine, interpret } from "xstate";
 
-const elOutput = document.querySelector('#output');
+const elOutput = document.querySelector("#output");
 
 function output(object) {
   elOutput.innerHTML = JSON.stringify(object, null, 2);
 }
 
-console.log('Welcome to the XState workshop!');
+console.log("Welcome to the XState workshop!");
 
 const user = {
-  name: 'David Khourshid',
-  company: 'Microsoft',
-  interests: ['piano', 'state machines'],
+  name: "David Khourshid",
+  company: "Microsoft",
+  interests: ["piano", "state machines"],
 };
 
 output(user);
+
+//
+
+const feedbackMachine = createMachine({
+  initial: "question",
+  states: {
+    question: {
+      on: {
+        CLICK_GOOD: {
+          target: "thanks",
+        },
+        CLICK_BAD: {
+          target: "form",
+        },
+      },
+    },
+    form: {
+      on: {
+        SUBMIT: "thanks",
+      },
+    },
+    thanks: {
+      on: {
+        CLOSE: "closed",
+      },
+    },
+    closed: {
+      type: "final",
+    },
+  },
+});
+
+const feedbackService = interpret(feedbackMachine);
+
+feedbackService.onTransition((state) => {
+  console.log(state.value);
+});
+
+feedbackService.start();
+
+feedbackService.send("CLICK_GOOD");
+
+feedbackService.stop();
--- src/03/index.js
+++ src/03/index.js
@@ -1,28 +1,41 @@
-import { createMachine, interpret } from 'xstate';
+import { createMachine, interpret } from "xstate";
 
-const elBox = document.querySelector('#box');
+const elBox = document.querySelector("#box");
 
 const machine = createMachine({
-  // Create your state machine here
-  // ...
+  initial: "inactive",
+  states: {
+    inactive: {
+      on: {
+        MOUSEDOWN: "active",
+      },
+    },
+    active: {
+      on: {
+        MOUSEUP: "inactive",
+      },
+    },
+  },
 });
 
 // Create a service using interpret(...)
-const service = undefined;
+const service = interpret(machine);
 
 // Listen to state transitions and set
 // `elBox.dataset.state` to the state value as before.
-// ...
-
-// Start the service.
-// ...
-
-elBox.addEventListener('mousedown', (event) => {
-  // Send a mousedown event
-  // ...
+service.onTransition((state) => {
+  elBox.dataset.state = state.value;
 });
 
-elBox.addEventListener('mouseup', (event) => {
+// Start the service.
+service.start();
+
+elBox.addEventListener("mousedown", (event) => {
+  // Send a mousedown event
+  service.send("MOUSEDOWN");
+});
+
+elBox.addEventListener("mouseup", (event) => {
   // Send a mouseup event
-  // ...
+  service.send("MOUSEUP");
 });
